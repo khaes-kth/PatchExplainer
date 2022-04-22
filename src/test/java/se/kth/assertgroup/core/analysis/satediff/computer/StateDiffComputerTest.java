2c0afac7976cb70e7f9422ec8ac60b84f49884c0
Khashayar Etemadi <khaes@kth.se>
2022-04-22 20:24:23 +0200

    first test is added

--- src/test/java/se/kth/assertgroup/core/analysis/satediff/computer/StateDiffComputerTest.java
+++ src/test/java/se/kth/assertgroup/core/analysis/satediff/computer/StateDiffComputerTest.java
@@ -1,23 +1,38 @@
 package se.kth.assertgroup.core.analysis.satediff.computer;
 
+import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
+import se.kth.assertgroup.core.analysis.statediff.computer.StateDiffComputer;
+import se.kth.assertgroup.core.analysis.statediff.models.ProgramStateDiff;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class StateDiffComputerTest {
 
     @Test
-    void computeStateDiff_diffIsGenerated(@TempDir Path tempDir) {
+    void computeStateDiff_diffIsGenerated(@TempDir Path tempDir) throws IOException, ParseException {
         Path simpleSahabDirectory = Paths.get("src/test/resources/sahab_reports/simple");
         File leftBreakpoint = simpleSahabDirectory.resolve("breakpoint/left.json").toFile(),
                 rightBreakpoint = simpleSahabDirectory.resolve("breakpoint/right.json").toFile(),
                 leftReturn = simpleSahabDirectory.resolve("return/right.json").toFile(),
-                rightReturn = simpleSahabDirectory.resolve("return/right.json").toFile();
+                rightReturn = simpleSahabDirectory.resolve("return/right.json").toFile(),
+                lineMapping = simpleSahabDirectory.resolve("project_data/line_mapping.csv").toFile(),
+                left_line_to_var = simpleSahabDirectory.resolve("project_data/left_line_to_var.csv").toFile(),
+                rightLineToVar = simpleSahabDirectory.resolve("project_data/right_line_to_var.csv").toFile();
 
-        for(int i = 1464; i <= 1482; i++)
-            System.out.println(i + "," + (i - 3));
+        StateDiffComputer sdc = new StateDiffComputer(
+                leftBreakpoint,
+                rightBreakpoint,
+                leftReturn,
+                rightReturn,
+                lineMapping,
+                left_line_to_var,
+                rightLineToVar);
+
+        ProgramStateDiff stateDiff = sdc.computeProgramStateDiff();
     }
 }
